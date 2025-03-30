c88d354c9c8e06ce5a270e6a5012e4148dbb2645
dekhil <ons.dekhil@esprit.tn>
2025-03-31 00:41:06 +0100

    initial commit

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "@/Components/AuthForm";
+
+const Page = () => {
+    return <AuthForm type="sign-up" />;
+};
+
+export default Page;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// Define props type for scalability
+const Page=()=>{
+    return <div>Home</div>
+}
+export default Page
